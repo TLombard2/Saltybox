9e1581287050e408e552bea824361552d545e3ad
Tyler Lombard <TylerLombard2@gmail.com>
2021-08-02 23:28:44 -0400

    Added a basic webserver with express and ejs.

--- web/app.js
+++ web/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const resolve = require('path').resolve;
+const ejs = require('ejs');
 const app = express();
-const port = 9000;
+const port = 61117;
+
+app.set('view engine', 'ejs');
+var filePath = resolve('./views/index'); //Obtains absolute path
 
 app.get('/', (req,res) => {
-    res.send('Hello World!')
+    res.render(filePath);
 })
 
-app.listen(port, () => {
-    console.log('Server Running!')
+const dataRouter = require('./routes/data');
+app.use('/data', dataRouter);
+
+app.listen(port, (err) => { //0.0.0.0
+    if (err) {
+        log.message('1: ' + err);
+    } else {
+    console.log('Server running!')
+    }
 })
---
+++ web/routes/data.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const resolve = require('path').resolve;
+const ejs = require('ejs');
+const router = express();
+
+var data = [];
+
+var db = new sqlite3.Database('../saltydb.db');
+
+db.all('SELECT * FROM fighterTable', (err,rows) => {
+    if (err) {
+        log.message('1: ' + err);
+    } else {
+        data = rows;
+        send(data);
+    }
+})
+
+function send(data) {
+    let nameRow = data.map( (value) => {
+        return value.name;
+    })
+    let winsRow = data.map( (value) => {
+        return value.wins;
+    })
+    let lossesRow = data.map( (value) => {
+        return value.losses;
+    })
+    let matchesRow = data.map( (value) => {
+        return value.matches;
+    })
+    let tournamentMatchWinsRow = data.map( (value) => {
+        return value.tournamentMatchWins;
+    })
+    let tournamentMatchLossesRow = data.map( (value) => {
+        return value.tournamentMatchLosses;
+    })
+    let tournamentMatchesRow = data.map( (value) => {
+        return value.tournamentMatches;
+    })
+    let tournamentFinalWinsRow = data.map( (value) => {
+        return value.tournamentFinalWins;
+    })
+    let favorRow = data.map( (value) => {
+        return value.favor;
+    })
+
+    var filePath = resolve('./views/data'); //Obtains absolute path
+    router.get('/', (req,res) => {
+        res.render(filePath, {data : {userQuery: req.params.userQuery, data}});
+    //createTable(nameRow, winsRow, lossesRow, matchesRow, tournamentMatchWinsRow, tournamentMatchLossesRow, tournamentMatchesRow, tournamentFinalWinsRow, favorRow);
+    }
+)}
+
+
+
+module.exports = router;
