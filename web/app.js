498b7004fc19d1a9967fdc410495a59f3dfdbe02
Tyler Lombard <TylerLombard2@gmail.com>
2021-08-06 17:19:09 -0400

    Added navbar to website.

--- web/app.js
+++ web/app.js
@@ -1,12 +1,19 @@
+const http = require('http')
+const https = require('https')
 const express = require('express');
 const resolve = require('path').resolve;
 const ejs = require('ejs');
 const app = express();
-const port = 61117;
+const fs = require('fs')
+
+var privateKey = fs.readFileSync('../key.pem', 'utf8');
+var certificate = fs.readFileSync('../cert.pem', 'utf8');
+
+var options = {key: privateKey, cert: certificate};
 
 app.set('view engine', 'ejs');
-var filePath = resolve('./views/index'); //Obtains absolute path
 
+var filePath = resolve('./views/index'); //Obtains absolute path
 app.get('/', (req,res) => { //Will load default page
     res.render(filePath);
 })
@@ -18,10 +25,11 @@ const jsonDataRouter = require('./routes/datajson'); //Will load /json/data.json
 app.use('/json/data.json', jsonDataRouter);
 
 
-app.listen(port, (err) => { //0.0.0.0
-    if (err) {
-        log.message('1: ' + err);
-    } else {
-    console.log('Server running!')
-    }
+http.createServer(app).listen(80, () => {
+    console.log("Listening on port 80!");
 })
+
+https.createServer(options, app).listen(443, () =>{
+    console.log("Listening on port 443!");
+})
+
